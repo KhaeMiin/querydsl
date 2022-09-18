96797ffc1c091457f1bff76e1e302c05f0e6ac9f
haemin <goals1005@naver.com>
2022-09-18 21:49:27 +0900

    join on절사용

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -303,4 +303,48 @@ public class QuerydslBasicTest {
                 .extracting("username")
                 .containsExactly("teamA", "teamB");
     }
+
+    /**
+     * 조인 on절
+     * 예) 회원과 팀을 조인하면서, 팀 이름이 teamA인 팀만 조인, 회원은 모두 조회
+     * JPQL: select m, t from Member m lsft join m.team t on t.name = 'teamA'
+     * outer join: 조회시 팀이 없는 경우 null로 채워서 출력됨 (구글에 검색하면 예시가 잘 나온다)
+     * inner: 팀이 없는 데이터는 제외되고 나온다.
+     */
+    @Test
+    void join_in_filtering() {
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+//                .leftJoin(member.team, team).on(team.name.eq("teamA")) //outer join경우 on절을 사용할 수 바께 없다.
+                .join(member.team, team).where(team.name.eq("teamA")) //inner join 경우 where이나 on절이나 결과가 같다 (그래서 그냥 where사용)
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
+
+
+    /**
+     * 연관관계 없는 엔티티 외부 조인
+     * 회원의 이름이 팀 이름과 같은 대상 외부 조인
+     */
+    @Test
+    void join_on_no_relation() {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .leftJoin(team).on(member.username.eq(team.name)) //맴버 이름과 팀 이름이 같은 경우 join, 같지 않으면 null (null 출력하지 않으려면 inner join 사용)
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
 }
