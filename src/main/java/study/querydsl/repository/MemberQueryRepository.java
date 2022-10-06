93924ff8950aac6263e031a5af3ba24dc2956d0e
haemin <goals1005@naver.com>
2022-10-06 13:47:48 +0900

    사용자 정의 Repository

---
+++ src/main/java/study/querydsl/repository/MemberQueryRepository.java
@@ -0,0 +1,69 @@
+package study.querydsl.repository;
+
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.stereotype.Repository;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
+import study.querydsl.dto.QMemberTeamDto;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+import static org.springframework.util.StringUtils.hasText;
+import static study.querydsl.entity.QMember.member;
+import static study.querydsl.entity.QTeam.team;
+
+/**
+ * Interface, Impl을 따로 만들지 않고 특화된 쿼리 경우 현재클래스처럼 바로 구현체로 만들어서 사용해도 된다
+ * (프로젝트가 커질 경우에도)
+ */
+@Repository
+public class MemberQueryRepository {
+
+
+    private final JPAQueryFactory queryFactory;
+
+    public MemberQueryRepository(EntityManager em) {
+        this.queryFactory = new JPAQueryFactory(em);
+    }
+
+    public List<MemberTeamDto> search(MemberSearchCondition condition) {
+        return queryFactory
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName")
+                ))
+                .from(member)
+                .leftJoin(member.team, team) //QTeam.team > static import
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe()))
+                .fetch();
+    }
+
+
+    private BooleanExpression usernameEq(String username) {
+        return hasText(username) ? member.username.eq(username) : null; //값이 있으면 비교해서 값 반환, 없으면 null반환
+    }
+
+    private Predicate teamNameEq(String teamName) {
+        return hasText(teamName) ? team.name.eq(teamName) : null ;
+    }
+
+    private BooleanExpression ageGoe(Integer ageGoe) {
+        return ageGoe != null ? member.age.goe(ageGoe) : null;
+
+    }
+
+    private BooleanExpression ageLoe(Integer ageLoe) {
+        return ageLoe != null ? member.age.loe(ageLoe) : null;
+    }
+
+}
